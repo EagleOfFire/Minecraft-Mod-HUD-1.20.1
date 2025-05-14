bfc0f986d6c383461361fe7f68534e74b02f9f1d
EagleOfFire <maxoucrafteur83@gmail.com>
2025-05-14 21:36:34 +0200

    new texture for hunger bar and size ajustment for health bar

--- src/main/java/ros/eagleoffire/roshud/HUDoverlay.java
+++ src/main/java/ros/eagleoffire/roshud/HUDoverlay.java
@@ -3,6 +3,7 @@ package ros.eagleoffire.roshud;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
@@ -16,17 +17,51 @@ public class HUDoverlay {
         if(event.getOverlay().id().getPath().equals("player_health")) {
             event.setCanceled(true);
 
-            Minecraft mc = Minecraft.getInstance();
             GuiGraphics guiGraphics = event.getGuiGraphics();
 
             ResourceLocation HEALTH_BAR = new ResourceLocation(ROSHUD.MODID, "textures/gui/interfacevie/emptylife.png");
             ResourceLocation GREEN_HEALTH = new ResourceLocation(ROSHUD.MODID, "textures/gui/interfacevie/greenlife.png");
 
-            int width = event.getGuiGraphics().guiWidth();
-            int height = (int)(width / 24.07142857142857);
+            //int width = event.getGuiGraphics().guiWidth();
+            int width = 200;
+            int height = 10;
 
-            guiGraphics.blit(GREEN_HEALTH, 0, 0, 0, 0, width, height, width, height);
-            guiGraphics.blit(HEALTH_BAR, 0, 0, 0, 0, width, height, width, height);
+            guiGraphics.blit(GREEN_HEALTH, 2, 2, 0, 0, width, height, width, height);
+            guiGraphics.blit(HEALTH_BAR, 2, 2, 0, 0, width, height, width, height);
+        }else if(event.getOverlay().id().getPath().equals("food_level")) {
+            event.setCanceled(true);
+            int foodLevel = 0;
+
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.player != null){
+                Player player = mc.player;
+
+                foodLevel = player.getFoodData().getFoodLevel();
+            }
+            GuiGraphics guiGraphics = event.getGuiGraphics();
+
+            ResourceLocation HUNGER_BAR = new ResourceLocation(ROSHUD.MODID, "textures/gui/interfacevie/foodbar.png");
+            ResourceLocation HUNGER_UNIT = new ResourceLocation(ROSHUD.MODID, "textures/gui/interfacevie/food.png");
+            ResourceLocation HUNGER_UNIT_HALF = new ResourceLocation(ROSHUD.MODID, "textures/gui/interfacevie/fooddemi.png");
+
+            int width = 200;
+            int height = 8;
+            int unit_width = 19;
+            int unit_heihgt = 5;
+
+            guiGraphics.blit(HUNGER_BAR, 2, 14, 0, 0, width, height, width, height);
+            for(int i=0; i<20; i++){
+                if (foodLevel > i){
+                    if (i % 2 == 0){
+                        guiGraphics.blit(HUNGER_UNIT, 2 + i * 10, 14, 0, 0, unit_width, unit_heihgt, unit_width, unit_heihgt);
+                    }else{
+                        guiGraphics.blit(HUNGER_UNIT_HALF, 2 + i * 10, 14, 0, 0, unit_width/2, unit_heihgt, unit_width/2, unit_heihgt);
+                    }
+                }else {
+                    break;
+                }
+            }
         }
+
     }
 }
